ad6d1deaf636a53b805e5f65c2ccd1755131eebe
vcolluto <86895240+vcolluto@users.noreply.github.com>
2023-04-14 13:02:23 +0200

    Add project files.

---
+++ Files/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using Files;
+
+List <Utente> utenti=new List<Utente>();	
+
+if (!File.Exists("dati\\utenti.txt"))
+    Console.WriteLine("File non trovato!");
+else
+{
+	try
+	{
+		StreamReader sr = File.OpenText("dati\\utenti.txt");  //apro il file in lettura (nella stessa cartella dell'eseguibile)
+
+		while (!sr.EndOfStream)      //finché ci sono dati da leggere
+		{
+			string? line = sr.ReadLine();   //leggo una riga dal file
+			if (line != null)
+			{
+                string[] campi = line.Split(' ');   //suddividi la stringa in più pezzi usando lo spazio come separatore
+				Console.WriteLine("UTENTE:");
+				Console.WriteLine($"\tNome: {campi[0]}");
+                Console.WriteLine($"\tCognome: {campi[1]}");
+                Console.WriteLine($"\tData di nascita: {campi[2]}");
+				utenti.Add(new Utente(campi[0], campi[1], DateOnly.ParseExact(campi[2],"d")));
+            }
+			
+		}
+		sr.Close();     //chiudo il file
+	}
+	catch (UnauthorizedAccessException)
+	{
+		Console.WriteLine("Non hai i diritti di accesso al file");
+	}
+	catch (Exception e)
+	{
+
+		Console.WriteLine("Si è verificato un problema: " + e.Message);
+	}
+}
+
+StreamWriter sw = File.CreateText("output.txt");
+sw.WriteLine("prova1");
+sw.WriteLine("prova2");
+sw.Close();
---
+++ Files/Utente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Files
+{
+    internal class Utente
+    {
+        public string? Nome { get; set; }
+        public string? Cognome { get; set; }
+        public DateOnly DataDiNascita { get; set; }
+
+        public Utente(string nome, string cognome, DateOnly dataDiNascita) 
+        { 
+            Nome = nome;
+            Cognome = cognome;
+            DataDiNascita = dataDiNascita;
+        }
+    }
+}
